889e1f940c40dee3e5dbd7809b3fa6907a664777
roadlinkbangladesh <roadlinkbangladesh@gmail.com>
2026-07-19 08:44:15 +0600

    Refactor fetch function to handle login route

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,13 +1,29 @@
-import { hashPassword } from "./utils/password.js";
+import { success, notFound, serverError } from "./utils/response.js";
+import { API } from "./config/constants.js";
+
+import { login } from "./routes/auth/login.js";
+
+const routes = {
+    [`POST:${API.AUTH}/login`]: login
+};
 
 export default {
-    async fetch() {
+    async fetch(request, env, ctx) {
+        try {
+            const url = new URL(request.url);
+            const key = `${request.method}:${url.pathname}`;
 
-        const hash = await hashPassword("roadlink123");
+            const handler = routes[key];
 
-        return Response.json({
-            hash
-        });
+            if (!handler) {
+                return notFound();
+            }
 
+            return await handler(request, env, ctx);
+
+        } catch (error) {
+            console.error(error);
+            return serverError();
+        }
     }
 };
