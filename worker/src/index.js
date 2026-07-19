c95bf6958846cd171fb288e0338e050a735e71b3
roadlinkbangladesh <roadlinkbangladesh@gmail.com>
2026-07-19 08:42:34 +0600

    Remove unused imports and login route

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,12 +1,3 @@
-import { success, notFound, serverError } from "./utils/response.js";
-import { API } from "./config/constants.js";
-
-import { login } from "./routes/auth/login.js";
-
-const routes = {
-    [`POST:${API.AUTH}/login`]: login
-};
-
 import { hashPassword } from "./utils/password.js";
 
 export default {
