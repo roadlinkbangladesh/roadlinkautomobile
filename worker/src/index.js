594c5c8182a40dde4c6ea4923115077f87c2961a
roadlinkbangladesh <roadlinkbangladesh@gmail.com>
2026-07-19 07:45:55 +0600

    Refactor password handling in fetch function
    Removed password hashing and verification logic, added salt length to response.

--- worker/src/index.js
+++ worker/src/index.js
@@ -12,24 +12,12 @@ import { hashPassword, verifyPassword } from "./utils/password.js";
 export default {
     async fetch() {
 
-        const password = "admin123";
-
-        const hash = await hashPassword(password);
-
-        const valid = await verifyPassword(
-            "admin123",
-            hash
-        );
-
-        const invalid = await verifyPassword(
-            "wrongpassword",
-            hash
-        );
+        const salt = crypto.getRandomValues(new Uint8Array(16));
 
         return Response.json({
-            hash,
-            valid,
-            invalid
+            success: true,
+            saltLength: salt.length
         });
+
     }
 };
