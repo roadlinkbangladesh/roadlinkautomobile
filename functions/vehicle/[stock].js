35d8ff81765d1cd6da238e213c31d007f8e1f2af
roadlinkbangladesh <roadlinkbangladesh@gmail.com>
2026-07-31 20:33:35 +0600

    Add files via upload

--- functions/vehicle/[stock].js
+++ functions/vehicle/[stock].js
@@ -1,1 +1,6 @@
+import { fetchAndInjectMetadata } from "../_utils/metadata-client.js";
 
+export async function onRequest(context) {
+  const stock = context.params.stock;
+  return fetchAndInjectMetadata(context, "vehicle", stock);
+}
