657cf7a4bc2d7b7510d7a8172d49c7d6dc9cfd1b
roadlinkbangladesh <roadlinkbangladesh@gmail.com>
2026-08-11 15:03:39 +0600

    Clear default settings in settings-loader.js
    Removed default values from company and SEO settings.

--- public/js/settings-loader.js
+++ public/js/settings-loader.js
@@ -6,7 +6,7 @@
 import { apiRequest, getPublicFileUrl, sanitizePhoneNumber, formatPhoneNumber } from "./shared/api.js";
 
 export const DEFAULT_SETTINGS = {
-  companyName: "Roadlink Automobiles",
+  companyName: "",
   address: "",
   phone: "",
   contactName: "",
@@ -18,9 +18,9 @@ export const DEFAULT_SETTINGS = {
   showEmail: true,
   facebookUrl: "",
   youtubeUrl: "",
-  seoTitleSuffix: "Roadlink Automobiles",
-  seoDefaultKeywords: "Japanese cars, reconditioned cars, Dhaka car importer",
-  seoDefaultDescription: "Roadlink Automobiles - Importer and seller of high-quality reconditioned Japanese vehicles in Dhaka, Bangladesh.",
+  seoTitleSuffix: "",
+  seoDefaultKeywords: "",
+  seoDefaultDescription: "",
   showSoldVehicles: true
 };
 
